7e2177d07bf7e674a39f41c619f8ac8831ad7574
Haroon Rafique <haroon.rafique@protonmail.com>
2019-01-22 16:21:25 +0100

    PTC-PyORBIT_Simulation_Scripts: Create_A_Bunch started

---
+++ PTC_PyORBIT_Simulation_Scripts/Create_A_Bunch/clean_folder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Clean folder for this specific PTC-PyORBIT Example:
+# Flat_file_from_madx
+rm -r bunch_output
+rm -r input
+rm -r lost
+rm -r output
+rm junk.txt
+rm Maxwellian_bend_for_ptc.txt
+rm ptc_twiss
+rm simulation_parameters.pyc
---
+++ PTC_PyORBIT_Simulation_Scripts/Create_A_Bunch/lib/save_bunch_as_matfile.py
@@ -0,0 +1,135 @@
+import orbit_mpi
+from orbit_mpi import mpi_datatype
+from orbit_mpi import mpi_op
+import numpy as np
+import scipy.io as sio
+from bunch import Bunch
+
+# 12.03.2017: works now also for lostbunch attributes
+# 03.06.2017: now a bunch object can be generated from a matfile
+
+def saveBunchAsMatfile(bunch, filename=None):
+
+	b = bunch
+	#take the MPI Communicator from bunch: it could be different from MPI_COMM_WORLD
+	comm = b.getMPIComm()
+	rank = orbit_mpi.MPI_Comm_rank(comm)
+	size = orbit_mpi.MPI_Comm_size(comm)	
+	main_rank = 0
+
+	# n_parts_arr - array of size of the number of CPUs, 
+	# and have the number of macroparticles on each CPU
+	n_parts_arr = [0]*size
+	n_parts_arr[rank] = b.getSize()
+	n_parts_arr = orbit_mpi.MPI_Allreduce(n_parts_arr,mpi_datatype.MPI_INT,mpi_op.MPI_SUM,comm)	
+
+	mp_array = range(n_parts_arr[rank])
+	particles = {}
+	particles['x'] = map(b.x, mp_array)
+	particles['xp'] = map(b.xp, mp_array)
+	particles['y'] = map(b.y, mp_array)
+	particles['yp'] = map(b.yp, mp_array)
+	particles['z'] = map(b.z, mp_array)
+	particles['dE'] = map(b.dE, mp_array)
+	phase_space_keys = particles.keys()
+
+	for attribute in b.getPartAttrNames():
+		particles[attribute] = [[] for i in range(b.getPartAttrSize(attribute))]
+		for j in xrange(b.getPartAttrSize(attribute)):
+			particles[attribute][j] += map(lambda i: b.partAttrValue(attribute, i, j), mp_array)
+
+	#This is just for case. Actually, MPI_Barrier command is not necessary.
+	orbit_mpi.MPI_Barrier(comm)
+
+	for i_cpu in range(1,size):
+		for key in phase_space_keys:
+			if(rank == main_rank):
+				#get the particle coordinates and attributes
+				bunch_size_remote = orbit_mpi.MPI_Recv(mpi_datatype.MPI_INT,i_cpu,222,comm)
+				if bunch_size_remote:
+					particles[key] += list(np.atleast_1d(orbit_mpi.MPI_Recv(mpi_datatype.MPI_DOUBLE,i_cpu,222,comm)))
+			elif(rank == i_cpu):
+				#send the coordinate array if there are any particles ...
+				bunch_size_local = bunch.getSize()
+				orbit_mpi.MPI_Send(bunch_size_local,mpi_datatype.MPI_INT,main_rank,222,comm)
+				if bunch_size_local:
+					orbit_mpi.MPI_Send(particles[key],mpi_datatype.MPI_DOUBLE,main_rank,222,comm)
+
+	for i_cpu in range(1,size):
+		for attribute in b.getPartAttrNames():
+			if(rank == main_rank):
+				bunch_size_remote = orbit_mpi.MPI_Recv(mpi_datatype.MPI_INT,i_cpu,222,comm)
+				if bunch_size_remote:
+					#get the particle coordinates and attributes
+					for j in xrange(b.getPartAttrSize(attribute)):
+						particles[attribute][j] += list(np.atleast_1d(orbit_mpi.MPI_Recv(mpi_datatype.MPI_DOUBLE,i_cpu,222,comm)))
+			elif(rank == i_cpu):
+				bunch_size_local = bunch.getSize()
+				orbit_mpi.MPI_Send(bunch_size_local,mpi_datatype.MPI_INT,main_rank,222,comm)
+				if bunch_size_local:
+					#send the coordinate array if there are any particles ...
+					for j in xrange(b.getPartAttrSize(attribute)):
+						orbit_mpi.MPI_Send(particles[attribute][j],mpi_datatype.MPI_DOUBLE,main_rank,222,comm)
+
+	bunchparameters = {'classical_radius': bunch.classicalRadius(), \
+					   'charge': bunch.charge(), 
+					   'mass': bunch.mass(), \
+					   'momentum': bunch.getSyncParticle().momentum(), \
+					   'beta': bunch.getSyncParticle().beta(), \
+					   'gamma': bunch.getSyncParticle().gamma(), \
+					   'time': bunch.getSyncParticle().time()}
+
+	if filename:
+		if rank == main_rank:
+			sio.savemat(filename + '.mat', {'particles': particles, 'bunchparameters': bunchparameters}, do_compression=True)
+	orbit_mpi.MPI_Barrier(comm)
+
+
+def bunch_from_matfile(matfile):
+	d = sio.loadmat(matfile, squeeze_me=True)
+	p = dict((key, value) for (key, value) in map(lambda k: (k, d['particles'][k][()]), d['particles'].dtype.names))
+	attributes = list(set(p) - set(['x', 'xp', 'y', 'yp', 'z', 'dE']))
+	attributes.sort(key=str.lower)
+
+	bunch = Bunch()
+	bunch.classicalRadius(d['bunchparameters']['classical_radius'])
+	bunch.charge(d['bunchparameters']['charge'])
+	bunch.mass(d['bunchparameters']['mass'])
+	bunch.getSyncParticle().momentum(d['bunchparameters']['momentum'])
+	bunch.getSyncParticle().time(d['bunchparameters']['time'])
+
+	x  = d['particles']['x'][()]
+	xp = d['particles']['xp'][()]
+	y  = d['particles']['y'][()]
+	yp = d['particles']['yp'][()]
+	z  = d['particles']['z'][()]
+	dE = d['particles']['dE'][()]
+	n_part = len(x)
+
+	import orbit_mpi
+	comm = bunch.getMPIComm()
+	rank = orbit_mpi.MPI_Comm_rank(comm)
+	size = orbit_mpi.MPI_Comm_size(comm)
+
+	count = n_part / size
+	remainder = n_part % size
+	if (rank < remainder):
+		i_start = rank * (count + 1)
+		i_stop = i_start + count+1
+	else:
+		i_start = rank * count + remainder
+		i_stop = i_start + count;
+	# print rank, i_start, i_stop
+
+	map(lambda i: bunch.addParticle(x[i],xp[i],y[i],yp[i],z[i],dE[i]), xrange(i_start,i_stop))
+	orbit_mpi.MPI_Barrier(comm)
+	for a in attributes:
+		bunch.addPartAttr(a)
+		a_size = bunch.getPartAttrSize(a)
+		if a_size>1:
+			for j in xrange(a_size):
+				map(lambda (ip,i): bunch.partAttrValue(a, ip, j, p[a][j][i]), enumerate(xrange(i_start,i_stop)))
+		else:
+			map(lambda (ip,i): bunch.partAttrValue(a, ip, 0, p[a][i]), enumerate(xrange(i_start,i_stop)))
+	orbit_mpi.MPI_Barrier(comm)
+	return bunch
