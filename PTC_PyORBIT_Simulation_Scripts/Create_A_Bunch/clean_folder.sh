4640df685468cbdc465dab8cf5082cf8abe297b3
Haroon Rafique <haroon.rafique@cern.ch>
2019-01-29 14:18:22 +0100

    PTC-PyORBIT_Simulation_Scripts: Create_A_Bunch updated

--- PTC_PyORBIT_Simulation_Scripts/Create_A_Bunch/clean_folder.sh
+++ PTC_PyORBIT_Simulation_Scripts/Create_A_Bunch/clean_folder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Clean folder for this specific PTC-PyORBIT Example:
 # Flat_file_from_madx
-rm - bunch_output
+rm -r bunch_output
 rm -r input
 rm -r lost
 rm -r output
@@ -9,3 +9,4 @@ rm junk.txt
 rm Maxwellian_bend_for_ptc.txt
 rm ptc_twiss
 rm simulation_parameters.pyc
+rm PTC-PyORBIT_flat_file.flt
